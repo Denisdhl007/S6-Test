0cd8599c6217adc88093087711f1b9036c42ef72
Denisdhl007 <denis.henin@icloud.com>
2023-01-16 09:56:47 +0100

    first commit

---
+++ public/script/script.js
@@ -0,0 +1,13 @@
+const questionnaire1_1 = document.getElementById("questionnaire1_1");
+function questionnaire1() {
+    questionnaire1_1.innerHTML = "Comment appelez vous votre Guerrier ?";
+}
+
+
+let submitButton = document.getElementById("mySubmit");
+let inputValue = document.getElementById("questionnaire2_1").value;
+
+submitButton.addEventListener("click", function() {
+  console.log(inputValue);
+  alert(inputValue);
+});
